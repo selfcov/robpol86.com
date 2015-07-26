1e9f7dfdb9dbea5d13355d47ed5c0d23dd91247e
Robpol86 <robpol86@gmail.com>
2015-07-26 14:35:01 -0700

    Fixed bash string slicing, github sources.

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,15 @@ version = release
 
 
 # Options for HTML output.
+html_context = dict(
+    conf_py_path='/docs/',
+    display_github=True,
+    github_repo=os.environ.get('TRAVIS_REPO_SLUG', '/robpol86.com').split('/', 1)[1],
+    github_user=os.environ.get('TRAVIS_REPO_SLUG', 'selfcov/').split('/', 1)[0],
+    github_version='master',
+    source_suffix='.rst',
+)
+html_copy_source = False
 html_favicon = 'favicon.ico'
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
