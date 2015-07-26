44ad26c0916986ab4ef1bc6778f9d9f5e5995969
Robpol86 <robpol86@gmail.com>
2015-07-25 23:36:59 -0700

    Slimmed down conf.py, favicon, download

--- docs/conf.py
+++ docs/conf.py
@@ -1,40 +1,19 @@
+import time
+
 import sphinx_rtd_theme
 
 
 # General configuration.
 author = 'Robpol86'
-copyright = '2015, Robpol86'
-exclude_patterns = ['_build']
-extensions = []
-language = None
+copyright = '{}, Robpol86'.format(time.strftime('%Y'))
 master_doc = 'index'
 project = 'Robpol86.com'
-pygments_style = 'sphinx'
 release = '1.0'
-source_suffix = '.rst'
-templates_path = ['_templates']
-todo_include_todos = False
-version = '1.0'
+version = release
 
 
 # Options for HTML output.
-html_static_path = ['_static']
+html_favicon = 'favicon.ico'
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-htmlhelp_basename = 'Robpol86comdoc'
-
-
-# Options for LaTeX output.
-latex_documents = [(master_doc, 'Robpol86com.tex', 'Robpol86.com Documentation', 'Robpol86', 'manual')]
-latex_elements = {}
-
-
-# Options for manual page output.
-man_pages = [(master_doc, 'robpol86com', 'Robpol86.com Documentation', [author], 1)]
-
-
-# Options for Texinfo output.
-texinfo_documents = [
-    (master_doc, 'Robpol86com', 'Robpol86.com Documentation', author, 'Robpol86com', 'One line description of project.',
-     'Miscellaneous'),
-]
+html_title = project
